30136f5fec519171db8d70a633e5b117880ef780
Sreeya Sai <sbs.sai@gmail.com>
2018-09-19 15:59:54 +0000

    deployed app

--- start.sh
+++ start.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-if [[ "x$PROD" == "x" ]]; then 
-	echo "This script is for starting in production."
-	echo "Use"
-	echo "   mix phx.server"
-	exit
-fi
+#if [[ "x$PROD" == "x" ]]; then 
+#	echo "This script is for starting in production."
+#	echo "Use"
+#	echo "   mix phx.server"
+#	exit
+#fi
 
-# TODO: Enable this script by removing the above.
 
 export MIX_ENV=prod
 export PORT=4790
@@ -18,9 +17,8 @@ _build/prod/rel/practice/bin/practice stop || true
 
 echo "Starting app..."
 
-_build/prod/rel/practice/bin/practice foreground
+_build/prod/rel/practice/bin/practice start
 
-# TODO: Change "foreground" to "Start"
 
 # TODO: Add a cron rule or systemd service file
 #       to start your app on system boot.
